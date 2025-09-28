ce2aa847cb08b51b518c8e09185b15bc04d858a7
manyeyes <zx.520.521@126.com>
2025-09-28 15:03:14 +0800

    update examples

---
+++ examples/ManySpeech.WenetAsr.Examples/Delegates/AsrCallbacks.cs
@@ -0,0 +1,7 @@
+﻿using ManySpeech.WenetAsr.Examples.Entities;
+
+namespace ManySpeech.WenetAsr.Examples.Delegates
+{
+    public delegate void RecognitionResultCallback(AsrResultEntity result);
+    public delegate void RecognitionCompletedCallback(TimeSpan totalTime, TimeSpan totalDuration, int processedCount, float[]? sample);
+}
---
+++ examples/ManySpeech.WenetAsr.Examples/Entities/AsrResultEntity.cs
@@ -0,0 +1,13 @@
+﻿namespace ManySpeech.WenetAsr.Examples.Entities
+{
+    public class AsrResultEntity
+    {
+        public string Text { get; set; }
+        public string[] Tokens { get; set; }
+        public int[][] Timestamps { get; set; }
+        public string[] Languages { get; set; }
+        public int Index { get; set; }
+        public string ModelName { get; set; }
+        public double ProcessingTimeMs { get; set; }
+    }
+}
