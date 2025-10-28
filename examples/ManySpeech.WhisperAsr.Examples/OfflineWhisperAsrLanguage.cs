372b3e1bfb134f007cda43477bfe06a5445ef5ed
manyeyes <zx.520.521@126.com>
2025-10-28 23:48:20 +0800

    code formating

--- examples/ManySpeech.WhisperAsr.Examples/OfflineWhisperAsrLanguage.cs
+++ examples/ManySpeech.WhisperAsr.Examples/OfflineWhisperAsrLanguage.cs
@@ -17,13 +17,7 @@ namespace ManySpeech.WhisperAsr.Examples
 
         public static void test_WhisperAsrLanguageDetection(List<float[]>? samples = null)
         {
-            //string modelName = "whisper-tiny-onnx";
-            //string modelName = "whisper-base-onnx";
-            string modelName = "whisper-small-onnx";
-            //string modelName = "whisper-medium-onnx";
-            //string modelName = "whisper-large-v1-onnx";
-            //string modelName = "whisper-large-v2-onnx";
-            //string modelName = "whisper-large-v3-finetune-onnx";
+            string modelName = "whisper-tiny-onnx";
             TimeSpan totalDuration = new TimeSpan(0L);
             List<List<float[]>> samplesList = new List<List<float[]>>();
             if (samples == null)
@@ -35,14 +29,7 @@ namespace ManySpeech.WhisperAsr.Examples
                     {
                         continue;
                     }
-                    // method 1
-                    //TimeSpan duration = TimeSpan.Zero;
-                    //float[] sample = AudioHelper.GetFileSample(wavFilePath, ref duration);
-                    //samples.Add(sample);
-                    //totalDuration += duration;
-                    //method 2
                     TimeSpan duration = TimeSpan.Zero;
-                    //float[] sample=AudioHelper.GetMediaSample(wavFilePath, ref duration);
                     float[] sample = AudioHelper.GetFileSample(wavFilePath, ref duration);
                     samples = new List<float[]>();
                     samples.Add(sample);
@@ -57,14 +44,6 @@ namespace ManySpeech.WhisperAsr.Examples
             LanguageDetection languageDetection = initWhisperAsrLanguageDetection(modelName);
             TimeSpan start_time = new TimeSpan(DateTime.Now.Ticks);            
             List<OfflineStream> streams = new List<OfflineStream>();
-            // method 1
-            //foreach (var sample in samples)
-            //{
-            //    OfflineStream stream = offlineRecognizer.CreateOfflineStream();
-            //    stream.AddSamples(sample);
-            //    streams.Add(stream);
-            //}
-            // method 2
             foreach (List<float[]> samplesListItem in samplesList)
             {
                 WhisperAsr.OfflineStream stream = languageDetection.CreateOfflineStream();
@@ -74,14 +53,6 @@ namespace ManySpeech.WhisperAsr.Examples
                 }
                 streams.Add(stream);
             }
-            // decode,fit batch=1
-            //foreach (WhisperAsr.OfflineStream stream in streams)
-            //{
-            //    WhisperAsr.Model.OfflineRecognizerResultEntity result = offlineRecognizer.GetResult(stream);
-            //    Console.WriteLine(result.Text);
-            //    Console.WriteLine("");
-            //}
-            //fit batch>1,but all in one
             List<OfflineRecognizerResultEntity> results_batch = languageDetection.GetResults(streams);
             foreach (OfflineRecognizerResultEntity result in results_batch)
             {
