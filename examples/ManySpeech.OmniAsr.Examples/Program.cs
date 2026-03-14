218072b8933114cb93b0ec6d9e07c8057ab5016d
manyeyes <zx.520.521@126.com>
2026-03-14 20:44:56 +0800

    Add examples for omniasr

---
+++ examples/ManySpeech.OmniAsr.Examples/Program.cs
@@ -0,0 +1,11 @@
+namespace ManySpeech.OmniAsr.Examples
+{
+    internal static partial class Program
+    {
+        [STAThread]
+        private static void Main()
+        {
+            OfflinneOmniAsrRecognizer.OfflineRecognizer();
+        }
+    }
+}
