e71349e76bc110183493733aeaedd99dcae07c1c
manyeyes <zx.520.521@126.com>
2026-03-30 22:18:49 +0800

    Add regularization

--- src/PreProcessUtils/AEDEmojiHelper.cs
+++ src/PreProcessUtils/AEDEmojiHelper.cs
@@ -6,6 +6,10 @@ namespace PreProcessUtils
     {
         public static string ReplaceTagsWithEmojis(string input)
         {
+            if (string.IsNullOrEmpty(input))
+            {
+                return input;
+            }
             // 定义标签与表情包的映射关系
             var emojiMap = new System.Collections.Generic.Dictionary<string, string>
             {
@@ -38,11 +42,26 @@ namespace PreProcessUtils
 
         public static string ReplaceTagsWithEmpty(string input)
         {
+            if (string.IsNullOrEmpty(input))
+            {
+                return input;
+            }
             string pattern = @"<\|.*?\|>";
             return Regex.Replace(input, pattern, match =>
             {
                 return "";
             });
+        }
+
+        public static string ReplaceTagsWithEmpty2(string input, string pattern = @"<.*?>")
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return input;
+            }
+            // 正则表达式：匹配 < 开头、> 结尾的任意字符（非贪婪匹配）
+            // \< 转义匹配 <，\> 转义匹配 >，.*? 非贪婪匹配中间任意字符
+            return Regex.Replace(input, pattern, "", RegexOptions.Singleline);
         }
     }
 }
